2e1f82dcc012764649005d88da123b58eb820068
JacksDK2102 <108046050+JacksDK2102@users.noreply.github.com>
2024-02-29 17:16:59 +1100

    updated SignupFor

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -126,7 +126,7 @@ const SignupForm = () => {
             )}
           />
           <Button type="submit" className="shad-button_primary">
-            {isCreatingAccount ? (
+            {isCreatingAccount || isSigningIn || isUserLoading ? (
               <div className="flex-center gap-2">
                 <Loader />
                 Loading...
--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -41,20 +41,21 @@ export const useSignOutAccount = () => {
 
 export const useGetPosts = () => {
     return useInfiniteQuery({
-        queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
-        queryFn: getInfinitePosts,
-        getNextPageParam: (lastPage) => {
-            // If there's no data, there are no more pages.
-            if(lastPage && lastPage.documents.length === 0) {
-                return null;
-            }
-
-            // Use the $id of the last document as the cursor.
-            const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
-            return lastId;
+      queryKey: [QUERY_KEYS.GET_INFINITE_POSTS],
+      queryFn: getInfinitePosts as any,
+      initialPageParam: undefined,
+      getNextPageParam: (lastPage: any) => {
+        // If there's no data, there are no more pages.
+        if (lastPage && lastPage.documents.length === 0) {
+          return null;
         }
-    })
-}
+  
+        // Use the $id of the last document as the cursor.
+        const lastId = lastPage.documents[lastPage.documents.length - 1].$id;
+        return lastId;
+      },
+    });
+  };
 
 export const useSearchPosts = (searchTerm: string) => {
     return useQuery({
