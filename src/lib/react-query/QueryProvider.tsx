c83267874e6d6d5fedb0c1e3ddfbd350874a16f3
JacksDK2102 <108046050+JacksDK2102@users.noreply.github.com>
2024-02-29 17:24:35 +1100

    updated codes

--- src/lib/react-query/QueryProvider.tsx
+++ src/lib/react-query/QueryProvider.tsx
@@ -1,5 +1,4 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { ReactNode } from 'react'
 
 const queryClient = new QueryClient();
@@ -8,7 +7,6 @@ export const QueryProvider = ({ children }: {children: ReactNode}) => {
   return (
     <QueryClientProvider client={queryClient}>
         { children }
-        {/*<ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   )
 }
